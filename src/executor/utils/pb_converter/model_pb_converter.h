3a5ca19f22772f36ad28e64c7d1e9464d0770641
yunchengwu <lemonwyc@gmail.com>
2020-08-13 12:12:22 +0800

    Add pb converter and unit test for model protobuf message

---
+++ src/executor/utils/pb_converter/model_pb_converter.h
@@ -0,0 +1,66 @@
+//
+// Created by wuyuncheng on 13/8/20.
+//
+
+#ifndef FALCON_SRC_EXECUTOR_UTILS_PB_CONVERTER_MODEL_PB_CONVERTER_H_
+#define FALCON_SRC_EXECUTOR_UTILS_PB_CONVERTER_MODEL_PB_CONVERTER_H_
+
+#include <string>
+
+/**
+ * serialize a model publish request protobuf message
+ *
+ * @param model_id: the target model id
+ * @param initiator_party_id: the party that sends the request
+ * @param output_message: serialized message to be sent
+ */
+void serialize_model_publish_request(int model_id,
+    int initiator_party_id,
+    std::string & output_message);
+
+/**
+ * deserialize model publish request from a protobuf message
+ *
+ * @param model_id: the target model id
+ * @param initiator_party_id: the party that sends the request
+ * @param input_message: received message
+ */
+void deserialize_model_publish_request(int & model_id,
+    int & initiator_party_id,
+    std::string input_message);
+
+/**
+ * serialize a model publish response protobuf message
+ *
+ * @param model_id: the target model id
+ * @param initiator_party_id: the party that sends the request
+ * @param is_success: whether the request is succeed
+ * @param error_code
+ * @param error_msg
+ * @param output_message: serialized message to be sent
+ */
+void serialize_model_publish_response(int model_id,
+    int initiator_party_id,
+    int is_success,
+    int error_code,
+    std::string error_msg,
+    std::string & output_message);
+
+/**
+ * deserialize model publish response from a protobuf message
+ *
+ * @param model_id: the target model id
+ * @param initiator_party_id: the party that sends the request
+ * @param is_success: whether the request is succeed
+ * @param error_code
+ * @param error_msg
+ * @param input_message: received message
+ */
+void deserialize_model_publish_response(int & model_id,
+    int & initiator_party_id,
+    int & is_success,
+    int & error_code,
+    std::string & error_msg,
+    std::string input_message);
+
+#endif //FALCON_SRC_EXECUTOR_UTILS_PB_CONVERTER_MODEL_PB_CONVERTER_H_
---
+++ test/falcon/test_pb_converter.cc
@@ -0,0 +1,67 @@
+//
+// Created by wuyuncheng on 13/8/20.
+//
+
+#include <string>
+#include <iostream>
+
+#include <gtest/gtest.h>
+#include "../../src/executor/utils/pb_converter/model_pb_converter.h"
+
+using namespace std;
+
+TEST(PB_Converter, ModelPublishRequest) {
+  int model_id = 1;
+  int initiator_party_id = 1001;
+  std::string output_message;
+  serialize_model_publish_request(model_id,
+      initiator_party_id,
+      output_message);
+
+  int deserialized_model_id;
+  int deserialized_initiator_party_id;
+  deserialize_model_publish_request(deserialized_model_id,
+      deserialized_initiator_party_id,
+      output_message);
+
+  EXPECT_EQ(1, deserialized_model_id);
+  EXPECT_EQ(1001, deserialized_initiator_party_id);
+}
+
+TEST(PB_Converter, ModelPublishResponse) {
+  int model_id = 1;
+  int initiator_party_id = 1001;
+  int is_success = 0;
+  int error_code = 2001;
+  std::string error_msg = "Model id does not exist.";
+  std::string output_message;
+  serialize_model_publish_response(model_id,
+      initiator_party_id,
+      is_success,
+      error_code,
+      error_msg,
+      output_message);
+
+  int deserialized_model_id;
+  int deserialized_initiator_party_id;
+  int deserialized_is_success;
+  int deserialized_error_code;
+  std::string deserialized_error_msg;
+  deserialize_model_publish_response(deserialized_model_id,
+      deserialized_initiator_party_id,
+      deserialized_is_success,
+      deserialized_error_code,
+      deserialized_error_msg,
+      output_message);
+
+  EXPECT_EQ(1, deserialized_model_id);
+  EXPECT_EQ(1001, deserialized_initiator_party_id);
+  EXPECT_EQ(0, deserialized_is_success);
+  EXPECT_EQ(2001, deserialized_error_code);
+  EXPECT_TRUE(error_msg == deserialized_error_msg);
+}
+
+int main(int argc, char **argv) {
+  testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
