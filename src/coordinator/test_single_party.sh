4c893b1d383899a2e5c4ed0506ac02724f86a3a8
nailixing <xnlainus809>
2020-12-18 13:47:32 +0800

    Update test scripts, change dev_test to .dev_test

--- src/coordinator/test_single_party.sh
+++ src/coordinator/test_single_party.sh
@@ -1,23 +1,23 @@
 # from base path
-cd src/coordinator/
+#cd src/coordinator/ || exit 1
 
 # setup party 1
 # mkdir for relevant resources
-mkdir dev_test
-cd dev_test
+mkdir .dev_test
+cd .dev_test || exit 1
 mkdir party1
-cd party1
+cd party1 || exit 1
 mkdir data_input
 mkdir data_output
 mkdir trained_model
 mkdir logs
 
 # setup coord folder
-cd ..
+cd .. || exit 1
 mkdir coord
 
 # launch coordinator FIRST!!
-bash scripts/dev_start_coord.sh build_linux
+#bash scripts/dev_start_coord.sh build_linux
 
 # then launch party
-bash scripts/dev_start_partyserver.sh build_linux
+#bash scripts/dev_start_partyserver.sh build_linux
