420773fb1325dcf598e9ff9c72d08ed91c507518
FeynmanDNA <26617036+FeynmanDNA@users.noreply.github.com>
2021-01-04 15:25:11 +0800

    local setup notes for mp-spdz

---
+++ tools/build_from_source/mp-spdz_setup.sh
@@ -0,0 +1,24 @@
+# for a source file in ./Programs/Source/, all SPDZ scripts must be run from ./.
+# The setup-online.sh script must also be run from ./ to create the relevant data.
+
+set -x
+set -e
+
+# Install third_party MP-SPDZ library
+# cd /opt/falcon
+cd third_party/MP-SPDZ
+mv Math/Setup.h.prod Math/Setup.h
+make -j 8 tldr
+
+# set up online phase
+bash fast-make.sh
+
+# set up ssl
+# Scripts/setup-ssl.sh 3
+Scripts/setup-clients.sh 3
+
+# compile high-level program
+./compile.py Programs/Source/logistic_regression.mpc
+
+
+# sudo ln -s /opt/falcon/third_party/MP-SPDZ/local/lib/libmpir* /usr/local/lib/
