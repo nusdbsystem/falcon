01c88df13c269ad385b9ab0edd31c209230470e5
yunchengwu <lemonwyc@gmail.com>
2021-03-08 21:44:25 +0800

    Update protoc generated files to align with grpc proto version

--- src/executor/inference/client/inference_client.cc
+++ src/executor/inference/client/inference_client.cc
@@ -2,3 +2,80 @@
 // Created by wuyuncheng on 8/3/21.
 //
 
+#include <iostream>
+#include <memory>
+#include <string>
+
+#include <grpcpp/grpcpp.h>
+#include "../../include/message/inference/lr_grpc.grpc.pb.h"
+
+using grpc::Channel;
+using grpc::ClientContext;
+using grpc::Status;
+using com::nus::dbsytem::falcon::v0::inference::PredictionRequest;
+using com::nus::dbsytem::falcon::v0::inference::PredictionResponse;
+using com::nus::dbsytem::falcon::v0::inference::InferenceLR;
+
+class LRInferenceClient {
+ public:
+  LRInferenceClient(std::shared_ptr<Channel> channel)
+      : stub_(InferenceLR::NewStub(channel)) {}
+
+  // Assembles the client's payload, sends it and presents the response back
+  // from the server.
+  std::string Prediction(int sample_num, int * sample_ids) {
+    // Data we are sending to the server.
+    PredictionRequest request;
+
+    // Container for the data we expect from the server.
+    PredictionResponse response;
+
+    // Context for the client. It could be used to convey extra information to
+    // the server and/or tweak certain RPC behaviors.
+    ClientContext context;
+
+    // The actual RPC.
+    Status status = stub_->Prediction(&context, request, &response);
+
+    // Act upon its status.
+    if (status.ok()) {
+      return "Success";
+    } else {
+      std::cout << status.error_code() << ": " << status.error_message() << std::endl;
+      return "RPC failed";
+    }
+  }
+
+ private:
+  std::unique_ptr<InferenceLR::Stub> stub_;
+};
+
+int main(int argc, char** argv) {
+  // Instantiate the client. It requires a channel, out of which the actual RPCs
+  // are created. This channel models a connection to an endpoint specified by
+  // the argument "--target=" which is the only expected argument.
+  // We indicate that the channel isn't authenticated (use of
+  // InsecureChannelCredentials()).
+  std::string target_str;
+  std::string arg_str("--target");
+  if (argc > 1) {
+    std::string arg_val = argv[1];
+    size_t start_pos = arg_val.find(arg_str);
+    if (start_pos != std::string::npos) {
+      start_pos += arg_str.size();
+      if (arg_val[start_pos] == '=') {
+        target_str = arg_val.substr(start_pos + 1);
+      } else {
+        std::cout << "The only correct argument syntax is --target=" << std::endl;
+        return 0;
+      }
+    } else {
+      std::cout << "The only acceptable argument is --target=" << std::endl;
+      return 0;
+    }
+  } else {
+    target_str = "localhost:50051";
+  }
+
+  return 0;
+}
