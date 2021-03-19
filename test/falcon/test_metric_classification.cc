2a012f791baf7c9079a7b653d6ff0369b4851d9f
FeynmanDNA <26617036+FeynmanDNA@users.noreply.github.com>
2021-03-19 19:03:12 +0800

    add f1 predictive value tests ClfMetrics

--- test/falcon/test_metric_classification.cc
+++ test/falcon/test_metric_classification.cc
@@ -116,5 +116,39 @@ TEST(Classification_metrics, test_balanced_acc) {
     EXPECT_FLOAT_EQ(ClfMetrics.balanced_accuracy, 0.6125);
 }
 
-  // F1 = 2TP / (2TP + FP + FN)
-  // F1 = 2 * (precision * recall) / (precision + recall)
+TEST(Classification_metrics, test_predictive_values) {
+    // Given a sample of 13 pictures, 8 of cats and 5 of dogs
+    // classifier makes 8 accurate predictions and misses 5
+    // 3 cats wrongly predicted as dogs (first 3 predictions)
+    // and 2 dogs wrongly predicted as cats (last 2 predictions). 
+    std::vector<int> predictions = {0,0,0,1,1,1,1,1,0,0,0,1,1};
+    std::vector<float> labels    = {1,1,1,1,1,1,1,1,0,0,0,0,0};
+
+    ClassificationMetrics ClfMetrics;
+    ClfMetrics.compute_metrics(predictions, labels);
+
+    EXPECT_EQ(ClfMetrics.precision, (double) 5/7);
+    EXPECT_EQ(ClfMetrics.NPV, (double) 3/6);
+}
+
+TEST(Classification_metrics, test_f1) {
+    // Given a sample of 13 pictures, 8 of cats and 5 of dogs
+    // classifier makes 8 accurate predictions and misses 5
+    // 3 cats wrongly predicted as dogs (first 3 predictions)
+    // and 2 dogs wrongly predicted as cats (last 2 predictions). 
+    std::vector<int> predictions = {0,0,0,1,1,1,1,1,0,0,0,1,1};
+    std::vector<float> labels    = {1,1,1,1,1,1,1,1,0,0,0,0,0};
+
+    ClassificationMetrics ClfMetrics;
+    ClfMetrics.compute_metrics(predictions, labels);
+
+    // F1 = 2TP / (2TP + FP + FN)
+    // F1 = 2 * (precision * recall) / (precision + recall)
+    EXPECT_EQ(ClfMetrics.F1, (double) 10/15);
+    EXPECT_EQ(
+        ClfMetrics.F1, 
+        ((double) 2 * ClfMetrics.precision * ClfMetrics.sensitivity\
+            / (ClfMetrics.precision + ClfMetrics.sensitivity)
+        )
+    );
+}
