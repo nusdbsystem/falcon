43f6ef8018925848f57e6450d75bfe7dbb914a05
nailixing <xingnaili14@gmail.com>
2021-10-10 19:11:02 +0800

    Update gitignore and make file

---
+++ make.sh
@@ -0,0 +1,18 @@
+
+# remove build folder
+rm -rf build
+
+# generate new protoc
+cp ~/.local/bin/protoc /usr/bin/ && \
+  cd /opt/falcon/src/executor/include/proto && \
+  SRC_DIR=v0/ && \
+  DST_DIR=../message && \
+  protoc -I=$SRC_DIR --cpp_out=$DST_DIR $SRC_DIR/*.proto
+
+# build project
+cd /opt/falcon
+export PATH="$PATH:$HOME/.local/bin" && \
+    mkdir build && \
+    cmake -Bbuild -H. && \
+    cd build/ && \
+    make
