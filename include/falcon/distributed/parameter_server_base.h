ed33e662fdf85bc4765663dac1b4dca8ac2d0151
nailixing <xingnaili14@gmail.com>
2021-10-10 19:50:51 +0800

    Add distributed train Worker and Parameter server base class

---
+++ include/falcon/distributed/parameter_server_base.h
@@ -0,0 +1,70 @@
+//
+// Created by nai li xing on 04/07/21.
+//
+
+#ifndef FALCON_PARAMETER_SERVER_H
+#define FALCON_PARAMETER_SERVER_H
+
+#include <falcon/network/Comm.hpp>
+#include <falcon/party/party.h>
+
+using namespace std;
+
+class ParameterServer {
+ public:
+  // boost i/o functionality
+  boost::asio::io_service io_service;
+  // communication channel between parameter server and follower
+  std::vector< shared_ptr<CommParty> > worker_channels;
+
+ public:
+  ParameterServer()=default;
+  explicit ParameterServer(const std::string& ps_network_config_pb_str);
+  ~ParameterServer();
+
+  /**
+   * send long message to worker via channel commParty
+   * @param message: sent message
+   */
+   void send_long_message_to_worker(int id, std::string message) const;
+
+  /**
+   * receive message from worker via channel comm_party
+   * @param message: received message
+   */
+  void recv_long_message_from_worker(int id, std::string& message) const;
+
+  /**
+   * abstract method of distributed_train
+   */
+  virtual void distributed_train()=0;
+
+  /**
+   * abstract distributed evaluation
+   *
+   * @param eval_type: train data or test data
+   * @param report_save_path: the path to save evaluation matrix
+   */
+  virtual void distributed_eval(falcon::DatasetType eval_type,
+      const std::string& report_save_path)=0;
+
+  /**
+   * abstract distributed prediction
+   *
+   * @param cur_test_data_indexes: vector of index
+   * @param predicted_labels: return value, array of labels
+   */
+  virtual void distributed_predict(
+      const std::vector<int>& cur_test_data_indexes,
+      EncodedNumber* predicted_labels)=0;
+
+  /**
+   * abstract method of saving the trained model
+   *
+   * @param model_save_file: vector of index
+   */
+  virtual void save_model(const std::string& model_save_file)=0;
+};
+
+
+#endif //FALCON_PARAMETER_SERVER_H
---
+++ include/falcon/distributed/worker.h
@@ -0,0 +1,38 @@
+//
+// Created by nai li xing on 11/08/21.
+//
+
+#ifndef FALCON_WORKER_H
+#define FALCON_WORKER_H
+
+
+class Worker {
+ public:
+  // Worker(Worker worker);
+  // communication channel between parameter server and follower
+  shared_ptr<CommParty> ps_channel;
+  // boost i/o functionality
+  boost::asio::io_service io_service;
+
+ public:
+
+  Worker()=default;
+  Worker(const std::string& ps_network_config_pb_str, int worker_id);
+  ~Worker();
+
+  /**
+   * send long message to parameter server via channel commParty
+   * @param message: sent message
+   */
+  void send_long_message_to_ps(string message) const;
+
+  /**
+   * receive message from parameter server via channel comm_party
+   * @param message: received message
+   */
+  void recv_long_message_from_ps(std::string& message) const;
+
+};
+
+
+#endif //FALCON_WORKER_H
