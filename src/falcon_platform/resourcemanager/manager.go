09b8348d693b52b36f2db68e4bd782938e86212d
nailixing <xingnaili14@gmail.com>
2021-10-10 18:56:28 +0800

    Update resourceManager to support docker runtime

--- src/falcon_platform/resourcemanager/manager.go
+++ src/falcon_platform/resourcemanager/manager.go
@@ -1,56 +1,112 @@
+// implement the abstract class of ResourceManager with interface + struct
 package resourcemanager
 
 import (
 	"context"
 	"falcon_platform/logger"
-	"os/exec"
 	"sync"
 )
 
+// interface for manager class
+type ManagerInterface interface {
+
+	// Monitor resource during resource runtime
+	ResourceMonitor(
+		resource interface{}, // resource need to be monitored
+		resourceClear context.Context, // control if clear resource or not
+		resourceRelease context.Context) // control if release resource or not
+
+	// Update deployment related config if necessary
+	UpdateConfig(config interface{})
+
+	// Create resources
+	CreateResource(
+		cmd interface{}, // command line to be executed
+		resourceClear context.Context, // control if clear resource or not
+		resourceRelease context.Context, // control if release resource or not
+		mux *sync.Mutex, // lock
+		resourceNum *int, // store number of sub-processes
+		TaskStatus *string, // store task status
+		runTimeErrorLog *string, // store task error logs
+	)
+
+	// delete resources
+	DeleteResource(resource interface{})
+}
+
 // subprocess/ will be named as resources
 type ResourceManager struct {
+	// public variables
+	// lock
 	Mux sync.Mutex
-
-	// total number of of resources(process or container)
-	TotResources int
-	// context
-	ResourceCtx context.Context
-	// kill all resources
-	ResourceClear context.CancelFunc
-
-	//task status
+	// task status, record status of tasks created by "ResourceManager.CreateResources"
 	TaskStatus string
-	//task executing error logs, if no error, it;s ""
+	// record executing error logs of task created by "ResourceManager.CreateResources",
+	// if no error, it;s ""
 	RunTimeErrorLog string
+
+	// private variables
+	// total number of of resources(process or container), used to check if there are running resources
+	resourceNum int
+	// clear resource or not
+	isClearCtx    context.Context
+	clearResource context.CancelFunc
+	// release resource or not,
+	isReleaseCtx    context.Context
+	releaseResource context.CancelFunc
 }
 
 func InitResourceManager() *ResourceManager {
-	tm := new(ResourceManager)
-	tm.TotResources = 0
-	// task manager hold a global context
-	tm.ResourceCtx, tm.ResourceClear = context.WithCancel(context.Background())
-	return tm
+	rm := new(ResourceManager)
+	// default resourceNum is 0
+	rm.resourceNum = 0
+	// a global context to control clear or not
+	rm.isClearCtx, rm.clearResource = context.WithCancel(context.Background())
+	// a global context to control if monitor resource anymore
+	rm.isReleaseCtx, rm.releaseResource = context.WithCancel(context.Background())
+	return rm
 }
 
-func (tm *ResourceManager) CreateResources(cmd *exec.Cmd) {
-	CreateProc(cmd, tm.ResourceCtx, &tm.Mux, &tm.TotResources, &tm.TaskStatus, &tm.RunTimeErrorLog)
-	logger.Log.Printf("[ResourceManager]: CreateResources error msg is: <'%s'> \n", tm.RunTimeErrorLog)
+/**
+ * @Description create resources, can be created by many ways. depend on mngr
+ * @Date 下午6:14 20/08/21
+ * @Param mngr: can be k8smanager, subprocessmngr or dockermanager
+ * @Param cmd: the cmd need to be executed, it's a string or cmd
+ * @return
+ **/
+func (rm *ResourceManager) CreateResources(mngr ManagerInterface, cmd interface{}) {
+
+	mngr.CreateResource(
+		cmd,
+		rm.isClearCtx, rm.isReleaseCtx,
+		&rm.Mux, &rm.resourceNum,
+		&rm.TaskStatus, &rm.RunTimeErrorLog)
+
+	if len(rm.RunTimeErrorLog) != 0 {
+		logger.Log.Printf("[ResourceManager]: CreateResources error msg is: \n============> \n%s \n<============\n", rm.RunTimeErrorLog)
+	}
 }
 
-func (tm *ResourceManager) DeleteResources() {
+func (rm *ResourceManager) DeleteResources() {
 
 	// check if there are running subprocess
-	tm.Mux.Lock()
+	rm.Mux.Lock()
 
 	// if there is still running job, kill the job
-	if tm.TotResources > 0 {
-		tm.Mux.Unlock()
-		// do the kill,  cancel all goroutines
-		tm.ResourceClear()
+	if rm.resourceNum > 0 {
+		rm.Mux.Unlock()
+		// do the kill, cancel all resources managed by this ResourceManager
+		rm.clearResource()
 		logger.Log.Printf("[ResourceManager]: Wait worker kill all process/containers")
 
 	} else {
-		tm.Mux.Unlock()
+		rm.Mux.Unlock()
 	}
 	logger.Log.Printf("[ResourceManager]: DeleteResources done")
+}
+
+// once release, ResourceManager can not delete it by calling rm.clearResource()
+func (rm *ResourceManager) ReleaseResources() {
+	logger.Log.Printf("[ResourceManager]: Release Resources, can not delete resources")
+	rm.releaseResource()
 }
--- src/falcon_platform/resourcemanager/mngr-process.go
+++ src/falcon_platform/resourcemanager/mngr-process.go
@@ -11,22 +11,31 @@ import (
 	"time"
 )
 
-func ProcMonitor(pid int, Ctx context.Context) {
+type SubProcessManager struct{}
+
+func InitSubProcessManager() *SubProcessManager {
+	subpMngr := new(SubProcessManager)
+	return subpMngr
+}
+
+func (mngr *SubProcessManager) ResourceMonitor(
+	resource interface{},
+	isClearCtx context.Context,
+	isRealeaseCtx context.Context,
+) {
 	// the ctx is shared by all threads created by worker,
 	// once cancel, all threads stopped
+	pid := resource.(int)
 loop:
 	for {
 		select {
-		case <-Ctx.Done():
-
+		case <-isRealeaseCtx.Done():
+			logger.Log.Println("[subProcessMngr]: resources released")
+			break
+		case <-isClearCtx.Done():
 			_, err := syscall.Getpgid(pid)
 			if err == nil {
-				err = syscall.Kill(pid, syscall.SIGQUIT)
-				if err != nil {
-					logger.Log.Println("[subProcessMngr]: Manually Killed PID=cmd.Process.Pid Error", err)
-				} else {
-					logger.Log.Println("[subProcessMngr]: Manually Killed PID=cmd.Process.Pid", pid)
-				}
+				mngr.DeleteResource(pid)
 			} else {
 				logger.Log.Printf("[subProcessMngr]: Clear resources, PID %d is not running, skip!\n", pid)
 			}
@@ -37,31 +46,36 @@ loop:
 	}
 }
 
-func CreateProc(
-	cmd *exec.Cmd, // command line to be executed
-	ctx context.Context, // control subprocess groups exist or not
+func (mngr *SubProcessManager) UpdateConfig(config interface{}) {
+
+}
+
+/**
+   * @Description
+   * @Date 11:54 上午 10/12/20
+   * @Param
+        // Each entry is of the form "key=value".
+        // If Env contains duplicate environment keys, only the last
+        // value in the slice for each duplicate key is used.
+        // As a special case on Windows, SYSTEMROOT is always added if
+        // missing and not explicitly set to the empty string.
+
+  	Dir specifies the working directory of the command.
+    	    If Dir is the empty string, Run runs the command in the
+    	    calling process's current directory.
+
+   * @return
+   **/
+func (mngr *SubProcessManager) CreateResource(
+	incmd interface{}, // command line to be executed
+	isClearCtx context.Context, // control if clear resource or not
+	isRealeaseCtx context.Context, // control if release resource or not
 	mux *sync.Mutex, // lock
 	TotResources *int, // store number of sub-processes
 	TaskStatus *string, // store task status
 	runTimeErrorLog *string, // store task error logs
 ) {
-	/**
-	   * @Author
-	   * @Description
-	   * @Date 11:54 上午 10/12/20
-	   * @Param
-	        // Each entry is of the form "key=value".
-	        // If Env contains duplicate environment keys, only the last
-	        // value in the slice for each duplicate key is used.
-	        // As a special case on Windows, SYSTEMROOT is always added if
-	        // missing and not explicitly set to the empty string.
-
-	  	Dir specifies the working directory of the command.
-	    	    If Dir is the empty string, Run runs the command in the
-	    	    calling process's current directory.
-
-	   * @return
-	   **/
+	var cmd *exec.Cmd = incmd.(*exec.Cmd)
 
 	defer func() {
 		//logger.Log.Println("[subProcessMngr]: lock")
@@ -71,6 +85,7 @@ func CreateProc(
 		//logger.Log.Println("[subProcessMngr]: Unlock ")
 	}()
 
+	//1. start pip to record standard error
 	stderr, err := cmd.StderrPipe()
 	if err != nil {
 		logger.Log.Println(err)
@@ -82,44 +97,56 @@ func CreateProc(
 	var out OutStream
 	cmd.Stdout = out
 
-	// start with a short statement to execute before the condition
+	// 2. start with a short statement to execute before the condition
 	if err := cmd.Start(); err != nil {
-		logger.Log.Println("cmd.Start() Error:")
-		logger.Log.Println(err)
+		logger.Log.Println("cmd.Start() Error:", err)
 		mux.Lock()
 		*runTimeErrorLog = err.Error()
 		mux.Unlock()
 		return
 	}
 
-	// if start successfully
-	if cmd.Process != nil {
+	// 3. if start not successfully, record error to TaskStatus
+	if cmd.Process == nil {
+		mux.Lock()
+		*TaskStatus = common.TaskFailed
+		mux.Unlock()
+		logger.Log.Printf("[subProcessMngr]: cmd.Process is Nil, start error")
+
+		// if start successfully
+	} else if cmd.Process != nil {
+		// record as running
 		mux.Lock()
 		*TaskStatus = common.TaskRunning
 		mux.Unlock()
 
 		logger.Log.Println("[subProcessMngr]: open subProcess, PID=", cmd.Process.Pid)
 
-		go ProcMonitor(cmd.Process.Pid, ctx)
+		// monitor the process
+		go func() {
+			defer logger.HandleErrors()
+			mngr.ResourceMonitor(cmd.Process.Pid, isClearCtx, isRealeaseCtx)
+		}()
+
 		// if there is a running SubProc, nTasks add 1
 		mux.Lock()
 		*TotResources += 1
 		mux.Unlock()
 
 		errLog, _ := ioutil.ReadAll(stderr)
-		executeStatus := cmd.Wait()
+		executeError := cmd.Wait()
 
-		if executeStatus != nil {
-			// this means subprocess exit with Non zero code
+		// this means subprocess exit with Non zero code
+		if executeError != nil {
 			mux.Lock()
 			*TaskStatus = common.TaskFailed
 			mux.Unlock()
-			logger.Log.Printf("[subProcessMngr]: subprocess exit status: << %s >> \n", executeStatus.Error())
-			logger.Log.Printf("[subProcessMngr]: subprocess error logs: \n"+
-				"<<<<<<<<<<\n "+
-				"%s \n"+
-				"<<<<<<<<<<\n",
-				string(errLog))
+			logger.Log.Printf("[subProcessMngr]: subprocess exit status: << %s >> \n", executeError.Error())
+			//logger.Log.Printf("[subProcessMngr]: subprocess error logs: \n"+
+			//	"<<<<<<<<<<\n "+
+			//	"%s \n"+
+			//	"<<<<<<<<<<\n",
+			//	string(errLog))
 			mux.Lock()
 			*runTimeErrorLog = string(errLog)
 			mux.Unlock()
@@ -129,11 +156,15 @@ func CreateProc(
 			*TaskStatus = common.TaskSuccessful
 			mux.Unlock()
 		}
+	}
+}
 
+func (mngr *SubProcessManager) DeleteResource(resource interface{}) {
+	pid := resource.(int)
+	err := syscall.Kill(pid, syscall.SIGQUIT)
+	if err != nil {
+		logger.Log.Println("[subProcessMngr]: Manually Killed PID=cmd.Process.Pid Error", err)
 	} else {
-		mux.Lock()
-		*TaskStatus = common.TaskFailed
-		mux.Unlock()
-		logger.Log.Printf("[subProcessMngr]: cmd.Process is Nil, start error")
+		logger.Log.Println("[subProcessMngr]: Manually Killed PID=cmd.Process.Pid", pid)
 	}
 }
