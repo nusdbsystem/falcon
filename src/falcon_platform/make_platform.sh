eabf068c76b8f8194c8bd964506a871b02c65698
nailixing <xingnaili14@gmail.com>
2021-10-10 19:00:02 +0800

    Add make_platform.sh

---
+++ src/falcon_platform/make_platform.sh
@@ -0,0 +1,9 @@
+unamestr=`uname`
+if [[ "$unamestr" == 'Linux' ]]; then
+   makeOS='build_linux'
+elif [[ "$unamestr" == 'Darwin' ]]; then
+   makeOS='build_mac'
+elif [[ "$unamestr" == 'WindowsNT' ]]; then
+   makeOS='build_windows'
+fi
+make $makeOS
