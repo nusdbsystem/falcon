9a2d2a534695714d02723adb74c25cf599496c6e
nailixing <xingnaili14@gmail.com>
2021-10-10 18:58:12 +0800

    Add max and min funcitons

--- src/falcon_platform/utils/utils.go
+++ src/falcon_platform/utils/utils.go
@@ -1,6 +1,7 @@
 package utils
 
 import (
+	"falcon_platform/common"
 	"io/ioutil"
 )
 
@@ -16,4 +17,26 @@ func WriteFile(strTest string, fileName string) error {
 	var d = []byte(strTest)
 	err := ioutil.WriteFile(fileName, d, 0666)
 	return err
+}
+
+func MinV(a []common.PortType) common.PortType {
+	cur := a[0]
+
+	for _, v := range a {
+		if cur > v {
+			cur = v
+		}
+	}
+	return cur
+}
+
+func MaxV(a []common.PortType) common.PortType {
+	cur := a[0]
+
+	for _, v := range a {
+		if cur < v {
+			cur = v
+		}
+	}
+	return cur
 }
