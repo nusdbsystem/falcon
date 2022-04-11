1246ab570b53318061b172c19804d12ddc0cd299
lemonviv <lemonwyc@gmail.com>
2022-04-11 21:42:17 +0800

    Revise auto para exp dsl

--- src/falcon_platform/run_batch_auto_parallelism_all.sh
+++ src/falcon_platform/run_batch_auto_parallelism_all.sh
@@ -8,17 +8,47 @@ sleep 1000
 bash scripts/docker_service_rm_all_container.sh
 
 # worker-1
-python3 coordinator_client.py --url 10.0.0.80:30004 -method submit -path ./examples/train_job_dsls/sigmod_experiments/efficiency/auto_parallelism/nSample4000/three_parties_partyfeature20_efficiency_lr_l1_w25.json
+python3 coordinator_client.py --url 10.0.0.80:30004 -method submit -path ./examples/train_job_dsls/sigmod_experiments/efficiency/auto_parallelism/nSample4000/three_parties_partyfeature20_efficiency_lr_l1_w22.json
+sleep 1000
+bash scripts/docker_service_rm_all_container.sh
+
+# worker-1
+python3 coordinator_client.py --url 10.0.0.80:30004 -method submit -path ./examples/train_job_dsls/sigmod_experiments/efficiency/auto_parallelism/nSample4000/three_parties_partyfeature20_efficiency_lr_l1_w24.json
+sleep 1000
+bash scripts/docker_service_rm_all_container.sh
+
+# worker-1
+python3 coordinator_client.py --url 10.0.0.80:30004 -method submit -path ./examples/train_job_dsls/sigmod_experiments/efficiency/auto_parallelism/nSample4000/three_parties_partyfeature20_efficiency_lr_l1_w26.json
+sleep 800
+bash scripts/docker_service_rm_all_container.sh
+
+# worker-1
+python3 coordinator_client.py --url 10.0.0.80:30004 -method submit -path ./examples/train_job_dsls/sigmod_experiments/efficiency/auto_parallelism/nSample4000/three_parties_partyfeature20_efficiency_lr_l1_w28.json
 sleep 800
 bash scripts/docker_service_rm_all_container.sh
 
 # worker-1
 python3 coordinator_client.py --url 10.0.0.80:30004 -method submit -path ./examples/train_job_dsls/sigmod_experiments/efficiency/auto_parallelism/nSample4000/three_parties_partyfeature20_efficiency_lr_l1_w30.json
-sleep 800
+sleep 600
 bash scripts/docker_service_rm_all_container.sh
 
 # worker-1
-python3 coordinator_client.py --url 10.0.0.80:30004 -method submit -path ./examples/train_job_dsls/sigmod_experiments/efficiency/auto_parallelism/nSample4000/three_parties_partyfeature20_efficiency_lr_l1_w35.json
+python3 coordinator_client.py --url 10.0.0.80:30004 -method submit -path ./examples/train_job_dsls/sigmod_experiments/efficiency/auto_parallelism/nSample4000/three_parties_partyfeature20_efficiency_lr_l1_w32.json
+sleep 600
+bash scripts/docker_service_rm_all_container.sh
+
+# worker-1
+python3 coordinator_client.py --url 10.0.0.80:30004 -method submit -path ./examples/train_job_dsls/sigmod_experiments/efficiency/auto_parallelism/nSample4000/three_parties_partyfeature20_efficiency_lr_l1_w34.json
+sleep 600
+bash scripts/docker_service_rm_all_container.sh
+
+# worker-1
+python3 coordinator_client.py --url 10.0.0.80:30004 -method submit -path ./examples/train_job_dsls/sigmod_experiments/efficiency/auto_parallelism/nSample4000/three_parties_partyfeature20_efficiency_lr_l1_w36.json
+sleep 600
+bash scripts/docker_service_rm_all_container.sh
+
+# worker-1
+python3 coordinator_client.py --url 10.0.0.80:30004 -method submit -path ./examples/train_job_dsls/sigmod_experiments/efficiency/auto_parallelism/nSample4000/three_parties_partyfeature20_efficiency_lr_l1_w38.json
 sleep 600
 bash scripts/docker_service_rm_all_container.sh
 
@@ -27,22 +57,61 @@ python3 coordinator_client.py --url 10.0.0.80:30004 -method submit -path ./examp
 sleep 600
 bash scripts/docker_service_rm_all_container.sh
 
+
+
+####### vary worker num #######
+
 # worker-1
-python3 coordinator_client.py --url 10.0.0.80:30004 -method submit -path ./examples/train_job_dsls/sigmod_experiments/efficiency/auto_parallelism/nSample4000/three_parties_partyfeature20_efficiency_lr_l1_w45.json
-sleep 600
+python3 coordinator_client.py --url 10.0.0.80:30004 -method submit -path ./examples/train_job_dsls/sigmod_experiments/efficiency/auto_parallelism/nSample4000Baseline/three_parties_partyfeature20_efficiency_lr_l1_w20.json
+sleep 1200
 bash scripts/docker_service_rm_all_container.sh
 
 # worker-1
-python3 coordinator_client.py --url 10.0.0.80:30004 -method submit -path ./examples/train_job_dsls/sigmod_experiments/efficiency/auto_parallelism/nSample4000/three_parties_partyfeature20_efficiency_lr_l1_w50.json
-sleep 600
+python3 coordinator_client.py --url 10.0.0.80:30004 -method submit -path ./examples/train_job_dsls/sigmod_experiments/efficiency/auto_parallelism/nSample4000Baseline/three_parties_partyfeature20_efficiency_lr_l1_w22.json
+sleep 1200
 bash scripts/docker_service_rm_all_container.sh
 
 # worker-1
-python3 coordinator_client.py --url 10.0.0.80:30004 -method submit -path ./examples/train_job_dsls/sigmod_experiments/efficiency/auto_parallelism/nSample4000/three_parties_partyfeature20_efficiency_lr_l1_w55.json
-sleep 600
+python3 coordinator_client.py --url 10.0.0.80:30004 -method submit -path ./examples/train_job_dsls/sigmod_experiments/efficiency/auto_parallelism/nSample4000Baseline/three_parties_partyfeature20_efficiency_lr_l1_w24.json
+sleep 1200
 bash scripts/docker_service_rm_all_container.sh
 
 # worker-1
-python3 coordinator_client.py --url 10.0.0.80:30004 -method submit -path ./examples/train_job_dsls/sigmod_experiments/efficiency/auto_parallelism/nSample4000/three_parties_partyfeature20_efficiency_lr_l1_w60.json
-sleep 600
+python3 coordinator_client.py --url 10.0.0.80:30004 -method submit -path ./examples/train_job_dsls/sigmod_experiments/efficiency/auto_parallelism/nSample4000Baseline/three_parties_partyfeature20_efficiency_lr_l1_w26.json
+sleep 1000
+bash scripts/docker_service_rm_all_container.sh
+
+# worker-1
+python3 coordinator_client.py --url 10.0.0.80:30004 -method submit -path ./examples/train_job_dsls/sigmod_experiments/efficiency/auto_parallelism/nSample4000Baseline/three_parties_partyfeature20_efficiency_lr_l1_w28.json
+sleep 1000
+bash scripts/docker_service_rm_all_container.sh
+
+# worker-1
+python3 coordinator_client.py --url 10.0.0.80:30004 -method submit -path ./examples/train_job_dsls/sigmod_experiments/efficiency/auto_parallelism/nSample4000Baseline/three_parties_partyfeature20_efficiency_lr_l1_w30.json
+sleep 800
+bash scripts/docker_service_rm_all_container.sh
+
+# worker-1
+python3 coordinator_client.py --url 10.0.0.80:30004 -method submit -path ./examples/train_job_dsls/sigmod_experiments/efficiency/auto_parallelism/nSample4000Baseline/three_parties_partyfeature20_efficiency_lr_l1_w32.json
+sleep 800
+bash scripts/docker_service_rm_all_container.sh
+
+# worker-1
+python3 coordinator_client.py --url 10.0.0.80:30004 -method submit -path ./examples/train_job_dsls/sigmod_experiments/efficiency/auto_parallelism/nSample4000Baseline/three_parties_partyfeature20_efficiency_lr_l1_w34.json
+sleep 800
+bash scripts/docker_service_rm_all_container.sh
+
+# worker-1
+python3 coordinator_client.py --url 10.0.0.80:30004 -method submit -path ./examples/train_job_dsls/sigmod_experiments/efficiency/auto_parallelism/nSample4000Baseline/three_parties_partyfeature20_efficiency_lr_l1_w36.json
+sleep 800
+bash scripts/docker_service_rm_all_container.sh
+
+# worker-1
+python3 coordinator_client.py --url 10.0.0.80:30004 -method submit -path ./examples/train_job_dsls/sigmod_experiments/efficiency/auto_parallelism/nSample4000Baseline/three_parties_partyfeature20_efficiency_lr_l1_w38.json
+sleep 800
+bash scripts/docker_service_rm_all_container.sh
+
+# worker-1
+python3 coordinator_client.py --url 10.0.0.80:30004 -method submit -path ./examples/train_job_dsls/sigmod_experiments/efficiency/auto_parallelism/nSample4000Baseline/three_parties_partyfeature20_efficiency_lr_l1_w40.json
+sleep 800
 bash scripts/docker_service_rm_all_container.sh
