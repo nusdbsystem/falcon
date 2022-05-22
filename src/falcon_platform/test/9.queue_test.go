fd4b1a1e75808b9a97831a0f4c791deb596e6dfc
NLGithubWP <xingnaili14@gmail.com>
2022-05-22 23:04:26 +0800

    Remove un-used test

--- src/falcon_platform/test/9.queue_test.go
+++ src/falcon_platform/test/9.queue_test.go
@@ -1,24 +1,9 @@
 package test
 
 import (
-	"falcon_platform/cache"
-	"fmt"
 	"testing"
 )
 
 func TestJobQueue(t *testing.T) {
-
-	a := cache.TrainJob{}
-
-	res := cache.Serialize(&a)
-
-	res2 := cache.Deserialize(res)
-
-	fmt.Println(res)
-
-	fmt.Println("---------------------------------------------------------------")
-
-	fmt.Println(res2.PartyAddrList)
-	fmt.Println(res2.JobId)
 
 }
