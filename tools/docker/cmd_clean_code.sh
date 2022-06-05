c37e50e0537a83ea7dabefdfff3f1a6ecf7fb024
NLGithubWP <xingnaili14@gmail.com>
2022-06-06 00:04:21 +0800

    Add new docker file to test

---
+++ tools/docker/cmd_clean_code.sh
@@ -0,0 +1,23 @@
+# checkout branch for test code
+echo "---------------git checkout branch--------"
+git branch
+git pull origin clean_code
+
+# make executor
+echo "---------------make executor--------"
+# bash make.sh
+
+# make platform
+echo "---------------make platform--------"
+
+cd ./src/falcon_platform
+
+export GOROOT=/usr/local/go
+export GOPATH=/gopath
+export PATH=$GOROOT/bin:$GOPATH/bin:$PATH
+export PATH=/root/.local/bin:$PATH
+
+bash make_platform.sh Linux
+
+echo "---------------run platform--------"
+./bin/falcon_platform
