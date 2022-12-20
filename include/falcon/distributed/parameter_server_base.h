5cd64c80de64523df037aa2a3469a761401dead8
NLGithubWP <xingnaili14@gmail.com>
2022-12-20 17:36:32 +0700

    Add broadcast_string_to_workers to base parameter server

--- include/falcon/distributed/parameter_server_base.h
+++ include/falcon/distributed/parameter_server_base.h
@@ -15,24 +15,30 @@ class ParameterServer {
   // boost i/o functionality
   boost::asio::io_service io_service;
   // communication channel between parameter server and follower
-  std::vector< shared_ptr<CommParty> > worker_channels;
+  std::vector<shared_ptr<CommParty> > worker_channels;
 
  public:
-  ParameterServer()=default;
-  explicit ParameterServer(const std::string& ps_network_config_pb_str);
-  ~ParameterServer();
+  ParameterServer() = default;
+  explicit ParameterServer(const std::string &ps_network_config_pb_str);
+  ~ParameterServer() = default;
 
   /**
    * send long message to worker via channel commParty
    * @param message: sent message
    */
-   void send_long_message_to_worker(int id, std::string message) const;
+  void send_long_message_to_worker(int id, std::string message) const;
 
   /**
    * receive message from worker via channel comm_party
    * @param message: received message
    */
-  void recv_long_message_from_worker(int id, std::string& message) const;
+  void recv_long_message_from_worker(int id, std::string &message) const;
+
+  /**
+   * PS broadcast the string to all registered workers
+   * @param serialized_str the string to be sent
+   */
+  void broadcast_string_to_workers(const string &serialized_str) const;
 
   /**
    * abstract method of distributed_train
@@ -46,7 +52,7 @@ class ParameterServer {
    * @param report_save_path: the path to save evaluation matrix
    */
   virtual void distributed_eval(falcon::DatasetType eval_type,
-      const std::string& report_save_path) = 0;
+                                const std::string &report_save_path) = 0;
 
   /**
    * abstract distributed prediction
@@ -55,16 +61,16 @@ class ParameterServer {
    * @param predicted_labels: return value, array of labels
    */
   virtual void distributed_predict(
-      const std::vector<int>& cur_test_data_indexes,
-      EncodedNumber* predicted_labels) = 0;
+      const std::vector<int> &cur_test_data_indexes,
+      EncodedNumber *predicted_labels) = 0;
 
   /**
    * abstract method of saving the trained model
    *
    * @param model_save_file: vector of index
    */
-  virtual void save_model(const std::string& model_save_file) = 0;
-};
+  virtual void save_model(const std::string &model_save_file) = 0;
 
+};
 
 #endif //FALCON_PARAMETER_SERVER_H
--- src/executor/distributed/parameter_server_base.cc
+++ src/executor/distributed/parameter_server_base.cc
@@ -7,50 +7,55 @@
 #include <glog/logging.h>
 #include <falcon/utils/logger/logger.h>
 
+ParameterServer::ParameterServer(const std::string &ps_network_config_pb_str) {
+  std::vector<std::string> worker_ips;
+  std::vector<int> worker_ports;
+  std::vector<std::string> ps_ips;
+  std::vector<int> ps_ports;
 
-ParameterServer::ParameterServer(const std::string& ps_network_config_pb_str){
-    std::vector<std::string> worker_ips;
-    std::vector< int > worker_ports;
-    std::vector<std::string> ps_ips;
-    std::vector<int> ps_ports;
+  // 1. read network config proto from coordinator
+  deserialize_ps_network_configs(
+      worker_ips, worker_ports,
+      ps_ips, ps_ports,
+      ps_network_config_pb_str);
+  log_info("Establish network communications with workers");
+  // establish communication connections between workers and parameter server
+  // current party instance is created at parameter server, build channel with workers
+  for (int wk_index = 0; wk_index < worker_ips.size(); wk_index++) {
+    SocketPartyData me_ps, other_worker;
+    me_ps = SocketPartyData(boost_ip::address::from_string(ps_ips[wk_index]),
+                            ps_ports[wk_index]);
+    other_worker = SocketPartyData(boost_ip::address::from_string(worker_ips[wk_index]),
+                                   worker_ports[wk_index]);
+    shared_ptr<CommParty> tmp_channel = make_shared<CommPartyTCPSynced>(io_service, me_ps, other_worker);
 
-    // 1. read network config proto from coordinator
-    deserialize_ps_network_configs(
-            worker_ips,worker_ports,
-            ps_ips, ps_ports,
-            ps_network_config_pb_str);
-    log_info("Establish network communications with workers");
-    // establish communication connections between workers and parameter server
-    // current party instance is created at parameter server, build channel with workers
-    for (int wk_index = 0; wk_index < worker_ips.size(); wk_index++){
-      SocketPartyData me_ps, other_worker;
-      me_ps = SocketPartyData(boost_ip::address::from_string(ps_ips[wk_index]),
-                                ps_ports[wk_index]);
-      other_worker = SocketPartyData(boost_ip::address::from_string(worker_ips[wk_index]),
-                                       worker_ports[wk_index]);
-      shared_ptr<CommParty> tmp_channel = make_shared<CommPartyTCPSynced>(io_service, me_ps, other_worker);
+    // connect to the other party and add channel
+    tmp_channel->join(500, 5000);
+    worker_channels.push_back(tmp_channel);
 
-      // connect to the other party and add channel
-      tmp_channel->join(500, 5000);
-      worker_channels.push_back(tmp_channel);
-
-      log_info("Communication channel established with worker index "
-        + std::to_string(wk_index) + ", ip is " + worker_ips[wk_index]
-        + ", port is " + std::to_string(worker_ports[wk_index]));
-    }
+    log_info("Communication channel established with worker index "
+                 + std::to_string(wk_index) + ", ip is " + worker_ips[wk_index]
+                 + ", port is " + std::to_string(worker_ports[wk_index]));
+  }
 }
 
-ParameterServer::~ParameterServer()= default;
-
-void ParameterServer::send_long_message_to_worker(int id,std::string message) const {
-    worker_channels[id]->writeWithSize(std::move(message));
+void ParameterServer::send_long_message_to_worker(int id, std::string message) const {
+  worker_channels[id]->writeWithSize(std::move(message));
 }
 
-void ParameterServer::recv_long_message_from_worker(int id, std::string& message) const {
-    vector<byte> recv_message;
-    worker_channels[id]->readWithSizeIntoVector(recv_message);
-    const byte* uc = &(recv_message[0]);
-    string recv_message_str(reinterpret_cast<char const*>(uc),
-                            recv_message.size());
-    message = recv_message_str;
+void ParameterServer::recv_long_message_from_worker(int id, std::string &message) const {
+  vector<byte> recv_message;
+  worker_channels[id]->readWithSizeIntoVector(recv_message);
+  const byte *uc = &(recv_message[0]);
+  string recv_message_str(reinterpret_cast<char const *>(uc),
+                          recv_message.size());
+  message = recv_message_str;
 }
+
+void ParameterServer::broadcast_string_to_workers(const std::string &serialized_str) const {
+  for (int wk_index = 0; wk_index < this->worker_channels.size(); wk_index++) {
+    this->send_long_message_to_worker(wk_index, serialized_str);
+  }
+}
+
+
